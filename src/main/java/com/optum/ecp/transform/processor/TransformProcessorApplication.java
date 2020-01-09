0bbb5fd5e3528cc9a23c4d580b33d9eb88f784b9
Tim Veil <3260845+timveil@users.noreply.github.com>
2020-01-09 17:47:44 -0500

    fixing bad parameter name

--- src/main/java/com/optum/ecp/transform/processor/TransformProcessorApplication.java
+++ src/main/java/com/optum/ecp/transform/processor/TransformProcessorApplication.java
@@ -3,26 +3,17 @@ package com.optum.ecp.transform.processor;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.postgresql.ds.PGConnectionPoolDataSource;
-import org.postgresql.ds.PGPoolingDataSource;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -43,14 +34,14 @@ public class TransformProcessorApplication {
 
 //        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 //        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&rewriteBatchedStatements=true");
+//        dataSource.setUrl("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&reWriteBatchedInserts=true");
 //        dataSource.setUsername("root");
 
         PGSimpleDataSource pds = new PGSimpleDataSource();
 //        pds.setServerNames(new String[]{"apvrp47071","apvrp47072","apvrp47074","apvrp47075","apvrp47076","apvrp47077","apvrp47081","apvrp47082","apvrp47083"});
 //        pds.setPortNumbers(new int[]{31475,31475,31475,31475,31475,31475,31475,31475,31475});
-        pds.setUrl("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&rewriteBatchedStatements=true");
-//        pds.setURL("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&rewriteBatchedStatements=true");
+        pds.setUrl("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&reWriteBatchedInserts=true");
+//        pds.setURL("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&reWriteBatchedInserts=true");
         pds.setDatabaseName("clinical");
         pds.setUser("root");
         pds.setReWriteBatchedInserts(true);
@@ -61,14 +52,14 @@ public class TransformProcessorApplication {
         config.setMinimumIdle(20);
         config.setPoolName("cockroach-spring-boot");
 //        config.setDriverClassName("org.postgresql.Driver");
-//        config.setJdbcUrl("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&rewriteBatchedStatements=true");
+//        config.setJdbcUrl("jdbc:postgresql://apvrp47071:31475,apvrp47072:31475,apvrp47074:31475,apvrp47075:31475,apvrp47076:31475,apvrp47077:31475,apvrp47081:31475,apvrp47082:31475,apvrp47083:31475/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&reWriteBatchedInserts=true");
 //        config.setUsername("root");
         config.setAutoCommit(true);
         Properties props = new Properties();
         props.setProperty("sslmode","disable");
         props.setProperty("loadBalanceHosts","true");
         props.setProperty("useServerPrepStmts","false");
-        props.setProperty("rewriteBatchedStatements","true");
+        props.setProperty("reWriteBatchedInserts","true");
         config.setDataSourceProperties(props);
         // We will wait for 15 seconds to get a connection from the pool.
         // Default is 30, but it shouldn't be taking that long.
@@ -89,7 +80,7 @@ public class TransformProcessorApplication {
         config.setMinimumIdle(20);
         config.setPoolName("yugabyte-spring-boot");
         config.setDriverClassName("org.postgresql.Driver");
-        config.setJdbcUrl("jdbc:postgresql://apvrp47071:32517,apvrp47072:32517,apvrp47074:32517,apvrp47075:32517,apvrp47076:32517,apvrp47077:32517,apvrp47081:32517,apvrp47082:32517,apvrp47083:32517/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&rewriteBatchedStatements=true");
+        config.setJdbcUrl("jdbc:postgresql://apvrp47071:32517,apvrp47072:32517,apvrp47074:32517,apvrp47075:32517,apvrp47076:32517,apvrp47077:32517,apvrp47081:32517,apvrp47082:32517,apvrp47083:32517/clinical?sslmode=disable&loadBalanceHosts=true&useServerPrepStmts=false&reWriteBatchedInsertsreWriteBatchedInserts=true");
         config.setUsername("postgres");
         config.setAutoCommit(true);
         // We will wait for 15 seconds to get a connection from the pool.
