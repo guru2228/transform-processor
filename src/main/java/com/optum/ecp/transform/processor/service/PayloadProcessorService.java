4f891bf498d36bcd305e956271afecd972d51ecf
Tim Veil <3260845+timveil@users.noreply.github.com>
2020-01-09 17:15:10 -0500

    adding service

--- src/main/java/com/optum/ecp/transform/processor/service/PayloadProcessorService.java
+++ src/main/java/com/optum/ecp/transform/processor/service/PayloadProcessorService.java
@@ -1,8 +1,6 @@
 package com.optum.ecp.transform.processor.service;
 
 import com.optum.ecp.transform.processor.config.ApplicationConfig;
-import com.optum.ecp.transform.processor.repo.NewSQLJdbcRepository;
-import com.optum.ecp.transform.processor.repo.OracleJdbcRepository;
 import org.apache.commons.collections4.ListUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -15,6 +13,10 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 public class PayloadProcessorService {
@@ -25,31 +27,60 @@ public class PayloadProcessorService {
 
     ApplicationConfig applicationConfig;
 
-    public PayloadProcessorService(SinkService sinkService, ApplicationConfig applicationConfig){
+    public PayloadProcessorService(SinkService sinkService, ApplicationConfig applicationConfig) {
         this.sinkService = sinkService;
         this.applicationConfig = applicationConfig;
     }
 
     @KafkaListener(topics = "${app.config.input-topic}", containerFactory = "kafkaListenerContainerFactory")
-    public void processPayload(ConsumerRecords<String, String> records, Acknowledgment ack)  {
+    public void processPayload(ConsumerRecords<String, String> records, Acknowledgment ack) {
+
+        Set<Map.Entry<String, Map<String, String>>> entries = applicationConfig.getQueries().entrySet();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(entries.size());
+
+        CountDownLatch latch = new CountDownLatch(entries.size());
+
         long startTime = System.currentTimeMillis();
+
         try {
             List<String> payloads = new ArrayList<String>();
             for (ConsumerRecord<String, String> record : records) {
                 payloads.add(record.key());
             }
-            LOGGER.info("Received the primary keys {} ",payloads.size() );
+            LOGGER.info("Received the primary keys {} ", payloads.size());
 
-            List<List<String>> subLists = ListUtils.partition(payloads, 10);
-            for (List<String> subList : subLists) {
-                for (Map.Entry<String, Map<String, String>> entry : applicationConfig.getQueries().entrySet()) {
-                    sinkService.selectAndInsert(entry.getKey(), entry.getValue(), subList);
-                }
+            // todo: increase in increments of 128 for additional testing
+            List<List<String>> subLists = ListUtils.partition(payloads, 128);
+            for (Map.Entry<String, Map<String, String>> entry : entries) {
+
+                executorService.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (List<String> subList : subLists) {
+                            String table = entry.getKey();
+                            sinkService.selectAndInsert(table, entry.getValue(), subList);
+                        }
+                        latch.countDown();
+                    }
+                });
+
             }
-            LOGGER.info("COMPLETED INSERTING SUCCESSFULLY {} rows IN {} ms ", (System.currentTimeMillis()-startTime));
+
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+
+            LOGGER.info("COMPLETED INSERTING SUCCESSFULLY {} rows IN {} ms ", (System.currentTimeMillis() - startTime));
             ack.acknowledge();
-        } catch (Exception e){
+
+
+        } catch (Exception e) {
             LOGGER.error("Exception occurred in processPayload ", e);
+        } finally {
+            executorService.shutdown();
         }
     }
 }
